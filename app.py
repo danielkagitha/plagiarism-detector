3ee8649cb4c0e1fb0db12b1a3b9ee022f32d39ad
danielkagitha <danielkagitha@gmail.com>
2025-11-03 15:22:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def upload_file():
     return render_template("results.html", results=results, filename=filename)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True) 
+
