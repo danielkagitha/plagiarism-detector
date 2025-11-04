5af4bdb8ebddcaa9b8499ffd46940dcb572b6267
danielkagitha <danielkagitha@gmail.com>
2025-11-04 21:45:01 +0530

    Change app.run to use host and port

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def upload_file():
     return render_template("results.html", results=results, filename=filename)
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(host="0.0.0.0", port=5000) 
 
